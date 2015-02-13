cbb871aff05f3e96b7de2d2c75d6592690e7f2b5
Dan Hipschman <dsh@vicarious.com>
2015-02-12 22:03:30 -0800

    add table with autolayout

---
+++ Yelp/BusinessCell.h
@@ -0,0 +1,16 @@
+//
+//  BusinessCell.h
+//  Yelp
+//
+//  Created by Dan Hipschman on 2/12/15.
+//  Copyright (c) 2015 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Business.h"
+
+@interface BusinessCell : UITableViewCell
+
+@property (strong, nonatomic) Business *business;
+
+@end
